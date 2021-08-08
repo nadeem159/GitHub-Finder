c4d4b658735254b50987a89a05692ba5a251844e
Nadeem Mansuri <n169@live.com>
2021-08-08 15:18:07 +0530

    project complete

--- github.js
+++ github.js
@@ -1,7 +1,22 @@
-class GitHub{
-    constructor(){
-        this.client_id = '0fdd7bf5a62ef68a71d9';
-        this.client_secret = 'b866feda7562625da9cb5ec32d2d328c50d8f43b';
-
+class Github {
+    constructor() {
+      this.client_id = '0fdd7bf5a62ef68a71d9';
+      this.client_secret = 'c871fb41aeb053c0b048620856b6bc779250810d';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
     }
-}
+  
+    async getUser(user) {
+      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const profile = await profileResponse.json();
+      const repos = await repoResponse.json();
+  
+      return {
+        profile,
+        repos
+      }
+    }
+  }
